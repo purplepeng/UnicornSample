53d06d9652875687bbfe6615eaa45346591a77dd
Luis Valencia <luis@Anas-iMac.local>
2013-03-03 19:01:45 -0800

    Initial Empty Project

---
+++ UnicornSampleTests/UnicornSampleTests.h
@@ -0,0 +1,13 @@
+//
+//  UnicornSampleTests.h
+//  UnicornSampleTests
+//
+//  Created by Luis Valencia on 3/3/13.
+//  Copyright (c) 2013 Luis Valencia. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnicornSampleTests : SenTestCase
+
+@end
---
+++ UnicornSampleTests/UnicornSampleTests.m
@@ -0,0 +1,32 @@
+//
+//  UnicornSampleTests.m
+//  UnicornSampleTests
+//
+//  Created by Luis Valencia on 3/3/13.
+//  Copyright (c) 2013 Luis Valencia. All rights reserved.
+//
+
+#import "UnicornSampleTests.h"
+
+@implementation UnicornSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnicornSampleTests");
+}
+
+@end
